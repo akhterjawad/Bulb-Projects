5438573504ddfacdabd294dae2a9449221f12aae
akhterjawad <akhterjawad566@gmail.com>
2024-05-24 21:08:05 +0500

    almost complete

---
+++ app.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('img')
+img.addEventListener('mouseover', function () {
+    img.src = './images/bulb on.webp'
+})
+img.addEventListener('mouseout', function () {
+    img.src = './images/word-image7.png'
+    // img.style.width= 100
+})
